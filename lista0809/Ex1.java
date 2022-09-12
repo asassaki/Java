d91d2b6ca394ad4dddf986cadb5abe2246d9c554
asassaki <alinesassaki@hotmail.com>
2022-09-12 11:03:22 -0300

    Alteração de texto do print

--- lista0809/Ex1.java
+++ lista0809/Ex1.java
@@ -13,22 +13,22 @@ import java.util.Scanner;
 			
 			Scanner leia = new Scanner(System.in);
 
-			System.out.println("\nEntre com a primeira nota: ");
+			System.out.println("\nInsira primeiro número: ");
 			n1 = leia.nextInt();
-			System.out.println("\nEntre com a segunda nota: ");
+			System.out.println("\nInsira segundo número: ");
 			n2 = leia.nextInt();
-			System.out.println("\nEntre com a terceira nota: ");
+			System.out.println("\nInsira terceiro número: ");
 			n3 = leia.nextInt();
 			
 			
 			if(n1>=n2 && n1>=n3) {
-				System.out.println("\nMaior pontuação: "+n1);
+				System.out.println("\nMaior número: "+n1);
 			}
 			else if(n2>=n1 && n2>=n3) {
-				System.out.println("\nMaior pontuação: "+n2);
+				System.out.println("\nMaior número: "+n2);
 			}
 			else {
-				System.out.println("\nMaior pontuação: "+n3);
+				System.out.println("\nMaior número: "+n3);
 				
 			}
 		}
