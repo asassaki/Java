d65ab9bba461ab052378f16d4d05e00468f1c748
asassaki <alinesassaki@hotmail.com>
2022-09-16 14:42:00 -0300

    Adicionando comentário do enunciado

--- lista1609/Ex3Collections.java
+++ lista1609/Ex3Collections.java
@@ -7,6 +7,13 @@ public class Ex3Collections {
 
 	public static void main(String[] args) {
 		
+		/*Crie uma um programa para trabalhar com estoque de uma loja, o programa deverá trabalhar com Collection do tipo List do Java para manipular os dados desse estoque, o programa deverá atender as seguintes funcionalidades:
+		Armazenar dados da List
+		Remover dados da list;
+		Atualizar dados da list.
+		Apresentar todos os dados da list.
+		 */
+		
 		int op;
 		Scanner ler = new Scanner(System.in);
 		
